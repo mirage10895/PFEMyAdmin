fbc08d402899f6db24eecd60b4ca268ed766fe76
Lucas Amiaud <lucas.amiaud@reseau.eseo.fr>
2018-01-17 15:36:52 +0100

    + added security for confidentiality

--- PFEProject/app/src/main/java/fr/eseo/dis/amiaudluc/pfeproject/subjects/AllSubjectsFragment.java
+++ PFEProject/app/src/main/java/fr/eseo/dis/amiaudluc/pfeproject/subjects/AllSubjectsFragment.java
@@ -12,6 +12,7 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import fr.eseo.dis.amiaudluc.pfeproject.Content.Content;
 import fr.eseo.dis.amiaudluc.pfeproject.R;
@@ -81,12 +82,17 @@ public class AllSubjectsFragment extends android.support.v4.app.Fragment impleme
     @Override
     public void onItemClick(int position) {
         Content.project = Content.projects.get(position);
-        if(Content.project.isPoster()) {
-            GetPoster mGetPostTask = new GetPoster(ctx);
-            mGetPostTask.execute();
+        if(Content.project.getConfidentiality() == 0) {
+            if (Content.project.isPoster()) {
+                GetPoster mGetPostTask = new GetPoster(ctx);
+                mGetPostTask.execute();
+            } else {
+                Intent intent = new Intent(ctx, SubjectActivity.class);
+                startActivity(intent);
+            }
         }else{
-            Intent intent = new Intent(ctx, SubjectActivity.class);
-            startActivity(intent);
+            Toast toast = Toast.makeText(ctx, "Access denied", Toast.LENGTH_SHORT);
+            toast.show();
         }
     }
 
